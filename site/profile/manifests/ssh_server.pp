dc297a0271c80b9befdf3366dfe9af529df0de90
r1chardb <58387549+r1chardb@users.noreply.github.com>
2019-12-01 17:58:42 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCx/Hi3r3kJo/J2g5hyBiVNcoaKja0NCoziXp4fmoiIe4dXOUvnH6q5nCX6y/3I9+u9fEeA7hengl0gP/ThrySeKXYARkNUdMJS1k6yfCg1Ik5KVGfR6Cwfv95bhsKChDoqq1h13WvAYuldI8aC+sLa3HH/Hll5+9zdT5NzBcNbshiAmad7V148vFljGIRoDFiUo003RdHDCUmwgc+8nACZpBBVxQIC8Cp7ysQyRSmH6Nf/q5cgIXDS77HSLAzD/MdbiwYg0RoGJ7ClTlEhyaJRllPuGAFmLePeUtyE5d7RkNM2CkWqu7MdnQu661WQpxHk9StBp/ANDBPehmgWR09J',
 	}  
 }
